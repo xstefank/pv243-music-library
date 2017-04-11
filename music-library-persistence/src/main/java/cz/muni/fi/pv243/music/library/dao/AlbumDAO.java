fbe6e2c925ad89d852f7ab8639cb17875c270111
xstefank <xstefank122@gmail.com>
2017-04-11 22:53:42 +0200

    add AlbumDAO implementation

---
+++ music-library-persistence/src/main/java/cz/muni/fi/pv243/music/library/dao/AlbumDAO.java
@@ -0,0 +1,28 @@
+package cz.muni.fi.pv243.music.library.dao;
+
+import cz.muni.fi.pv243.music.library.entity.Album;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:xstefank122@gmail.com">Martin Stefanko</a>
+ */
+public interface AlbumDAO extends GenericDAO<Album, Long> {
+
+    /**
+     * Returns all albums containing given title fragment in their title
+     *
+     * @param titleFragment title fragment of albums to look for
+     * @return list of all album entities containing given title fragment , empty if no such album exists
+     */
+    List<Album> searchByTitle(String titleFragment);
+
+    /**
+     * Returns the sample of albums stored in the DB. Returns up
+     * to count albums or less if there is no sufficient ammount.
+     *
+     * @param count number of albums to return
+     * @return list of sample albums
+     */
+    List<Album> getAlbumSample(int count);
+}
---
+++ music-library-persistence/src/main/java/cz/muni/fi/pv243/music/library/dao/AlbumDAOImpl.java
@@ -0,0 +1,63 @@
+package cz.muni.fi.pv243.music.library.dao;
+
+import cz.muni.fi.pv243.music.library.dao.qualifier.JPADAO;
+import cz.muni.fi.pv243.music.library.entity.Album;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:xstefank122@gmail.com">Martin Stefanko</a>
+ */
+@JPADAO
+public class AlbumDAOImpl extends BaseJPADAO implements AlbumDAO {
+
+    @Override
+    public void create(Album album) {
+        getEntityManager().persist(album);
+    }
+
+    @Override
+    public Album update(Album album) {
+        return getEntityManager().merge(album);
+    }
+
+    @Override
+    public void remove(Long id) {
+        getEntityManager().remove(find(id));
+    }
+
+    @Override
+    public Album find(Long id) {
+        return getEntityManager().find(Album.class, id);
+    }
+
+    @Override
+    public List<Album> searchByTitle(String titleFragment) {
+        if(titleFragment == null) {
+            throw new IllegalArgumentException("titleFragment cannot be null.");
+        }
+        return getEntityManager()
+                .createQuery("SELECT a FROM Album a WHERE UPPER(a.title) LIKE '%'||:titleFragment||'%'", Album.class)
+                .setParameter("titleFragment", titleFragment.toUpperCase())
+                .getResultList();
+    }
+
+    @Override
+    public List<Album> findAll() {
+        return getEntityManager().createQuery("SELECT a FROM Album a", Album.class)
+                .getResultList();
+    }
+
+    @Override
+    public List<Album> getAlbumSample(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("count must be a possitive number");
+        }
+
+        TypedQuery<Album> query = getEntityManager()
+                .createQuery("SELECT a FROM Album a ORDER BY RANDOM()", Album.class);
+        query.setMaxResults(count);
+        return query.getResultList();
+    }
+}
