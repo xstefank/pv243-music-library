9ecd00d8e410042711afcfb52d92b6fdf3d5f7bc
xstefank <xstefank122@gmail.com>
2017-04-27 22:26:45 +0200

    add config scripts for 2 standalone servers domain

---
+++ config/cleanup.sh
@@ -0,0 +1,10 @@
+
+rm -rf cz.muni.fi.pv243.music.library.entity.Album
+rm -rf cz.muni.fi.pv243.music.library.entity.Artist
+rm -rf cz.muni.fi.pv243.music.library.entity.Genre
+rm -rf cz.muni.fi.pv243.music.library.entity.Song
+rm -rf cz.muni.fi.pv243.music.library.entity.User
+
+rm -rf server1
+rm -rf server2
+
---
+++ config/setupCluster2Nodes.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+WF_HOME=$1
+
+WF_HOME1=./server1
+WF_HOME2=./server2
+
+echo "Removing server directories..."
+rm -rf $WF_HOME1
+rm -rf $WF_HOME2
+
+echo "Creating server directories..."
+cp $WF_HOME $WF_HOME1 -r 
+cp $WF_HOME $WF_HOME2 -r
+
+echo "Starting H2 Database..."
+gnome-terminal -e "java -jar ../h2-1.4.195.jar -webAllowOthers -tcpAllowOthers"
+
+echo "Starting servers..."
+gnome-terminal -e "$WF_HOME1/bin/standalone.sh -c standalone-full-ha.xml -Djboss.node.name=node1"
+gnome-terminal -e "$WF_HOME2/bin/standalone.sh -c standalone-full-ha.xml -Djboss.node.name=node2 -Djboss.socket.binding.port-offset=100"
+
+echo "Waiting for servers to start..."
+sleep 20
+
+echo "Configuring servers..."
+$WF_HOME1/bin/jboss-cli.sh --file=./configStandaloneCluster.cli
+$WF_HOME2/bin/jboss-cli.sh --file=./configStandaloneCluster.cli --controller=localhost:10090
+
