e858078a437fc6fb7d9a5f092c1e23b2ad26ab76
Lenka Heldová <lenka.heldova@unicornsystems.eu>
2017-04-27 21:10:28 +0200

    Add date format validation + styling

--- music-library-web/src/main/webapp/partials/editAlbum.js
+++ music-library-web/src/main/webapp/partials/editAlbum.js
@@ -6,17 +6,18 @@ angular.module('app')
         $scope.album = {};
 
         $scope.artists = [];
-        $scope.doing = 'create';
+        $scope.doing = 'Create';
         $scope.changed = false;
+        $scope.dateRegex = '(((19|20)([2468][048]|[13579][26]|0[48])|2000)[\-]02[\-]29|((19|20)[0-9]{2}[\-](0[4678]|1[02])[\-](0[1-9]|[12][0-9]|30)|(19|20)[0-9]{2}[\-](0[1359]|11)[\-](0[1-9]|[12][0-9]|3[01])|(19|20)[0-9]{2}[\-]02[\-](0[1-9]|1[0-9]|2[0-8])))';
         if ($routeParams.id) {
             commonTools.getAlbum($routeParams.id).then(function (response) {
                 $scope.album = response;
                 $scope.oldAlbum = angular.copy($scope.album);
             }, function (response) {
-                $scope.alerts.push({type: 'danger', title: 'Error '+ response.status, msg: response.statusText});
+                $scope.alerts.push({type: 'danger', title: 'Error ' + response.status, msg: response.statusText});
             });
 
-            $scope.doing = 'update';
+            $scope.doing = 'Update';
         }
 
         commonTools.getArtists().then(function (response) {
@@ -24,8 +25,8 @@ angular.module('app')
 
         });
 
-        $scope.update = function() {
-            if ($scope.doing === 'create') {
+        $scope.update = function () {
+            if ($scope.doing === 'Create') {
                 $http({
                     url: '/music/api/album',
                     method: "POST",
@@ -35,7 +36,11 @@ angular.module('app')
                     createUpdateTools.setAlerts([{type: 'success', title: 'Successful!', msg: $scope.status}]);
                     $location.path("/albumsOverview");
                 }, function (response) {
-                    $scope.alerts.push({type: 'danger', title: 'Cannot update album! Error '+ response.status, msg: response.statusText});
+                    $scope.alerts.push({
+                        type: 'danger',
+                        title: 'Cannot update album! Error ' + response.status,
+                        msg: response.statusText
+                    });
                 });
             } else {
                 $scope.messageBuilder = 'You have successfully updated these fields [';
@@ -62,19 +67,27 @@ angular.module('app')
                         data: $scope.album
                     }).then(function (response) {
                         $scope.status = $scope.messageBuilder.substring(0, $scope.messageBuilder.length - 2) + "] of album.";
-                        createUpdateTools.setAlerts([{type: 'success', title:'Successfull!', msg: $scope.status}]);
+                        createUpdateTools.setAlerts([{type: 'success', title: 'Successfull!', msg: $scope.status}]);
                         $location.path("/albumsOverview");
                     }, function (response) {
-                        $scope.alerts.push({type: 'danger', title: 'Cannot update album! Error '+ response.status, msg: response.statusText});
+                        $scope.alerts.push({
+                            type: 'danger',
+                            title: 'Cannot update album! Error ' + response.status,
+                            msg: response.statusText
+                        });
                     });
                 } else {
-                    createUpdateTools.setAlerts([{type: 'info', title: "No change!", msg: 'There have been no changes.'}]);
+                    createUpdateTools.setAlerts([{
+                        type: 'info',
+                        title: "No change!",
+                        msg: 'There have been no changes.'
+                    }]);
                     $location.path("/albumsOverview");
                 }
             }
         };
 
-        $scope.reset = function(form) {
+        $scope.reset = function (form) {
             if (form) {
                 form.$setPristine();
                 form.$setUntouched();
@@ -83,26 +96,34 @@ angular.module('app')
         };
 
         $scope.removeAlbum = function () {
-            if(!$scope.album) {
+            if (!$scope.album) {
                 confirm('Cannot remove Album immediately.\nAlbum probably have not been saved yet.');
                 $location.path("/albumsOverview");
             } else {
-                if(confirm('You are above to completely remove Album:\n'+ $scope.album.title +'\n\nAre you sure?')) {
+                if (confirm('You are above to completely remove Album:\n' + $scope.album.title + '\n\nAre you sure?')) {
                     $http({
                         url: '/music/api/album/' + $scope.album.id,
                         method: "DELETE"
                     }).then(function () {
-                        createUpdateTools.setAlerts([{type: 'success', title:'Removed!', msg: 'You have successfully deleted '+ $scope.album.title +' from system.'}]);
+                        createUpdateTools.setAlerts([{
+                            type: 'success',
+                            title: 'Removed!',
+                            msg: 'You have successfully deleted ' + $scope.album.title + ' from system.'
+                        }]);
                         $location.path("/albumsOverview");
                     }, function () {
-                        createUpdateTools.setAlerts([{type: 'danger', title:'Cannot Remove!', msg: $scope.album.title +' still has some songs assigned.'}]);
+                        createUpdateTools.setAlerts([{
+                            type: 'danger',
+                            title: 'Cannot Remove!',
+                            msg: $scope.album.title + ' still has some songs assigned.'
+                        }]);
                         $location.path("/albumsOverview");
                     })
                 }
             }
         };
 
-        $scope.closeAlert = function(index) {
+        $scope.closeAlert = function (index) {
             $scope.alerts.splice(index, 1);
         };
 
--- music-library-web/src/main/webapp/partials/editArtist.js
+++ music-library-web/src/main/webapp/partials/editArtist.js
@@ -1,35 +1,36 @@
 'use strict';
 
 angular.module('app')
-    .controller('editArtistCtrl', ['$scope', '$http', '$location', '$routeParams', 'commonTools', 'createUpdateTools', function ($scope, $http, $location, $routeParams,commonTools, createUpdateTools) {
+    .controller('editArtistCtrl', ['$scope', '$http', '$location', '$routeParams', 'commonTools', 'createUpdateTools', function ($scope, $http, $location, $routeParams, commonTools, createUpdateTools) {
 
         $scope.alerts = [];
         $scope.artist = {};
         $scope.changed = false;
-        $scope.doing = 'create';
+        $scope.doing = 'Create';
+        $scope.dateRegex = '(((19|20)([2468][048]|[13579][26]|0[48])|2000)[\-]02[\-]29|((19|20)[0-9]{2}[\-](0[4678]|1[02])[\-](0[1-9]|[12][0-9]|30)|(19|20)[0-9]{2}[\-](0[1359]|11)[\-](0[1-9]|[12][0-9]|3[01])|(19|20)[0-9]{2}[\-]02[\-](0[1-9]|1[0-9]|2[0-8])))';
         if ($routeParams.id) {
             commonTools.getArtist($routeParams.id).then(function (response) {
                 $scope.artist = response;
                 $scope.oldArtist = angular.copy($scope.artist);
             }, function (response) {
-                $scope.alerts.push({type: 'danger', title: 'Error '+ response.status, msg: response.statusText});
+                $scope.alerts.push({type: 'danger', title: 'Error ' + response.status, msg: response.statusText});
             });
 
-            $scope.doing = 'update';
+            $scope.doing = 'Update';
         }
 
-        $scope.update = function() {
-            if ($scope.doing === 'create') {
+        $scope.update = function () {
+            if ($scope.doing === 'Create') {
                 $http({
                     url: '/music/api/artist/',
                     method: "POST",
                     data: $scope.artist
-            }).then(function (response) {
+                }).then(function (response) {
                     $scope.status = "New artist successfully created.";
                     createUpdateTools.setAlerts([{type: 'success', title: 'Successful!', msg: $scope.status}]);
                     $location.path("/artistsOverview");
                 }, function (response) {
-                    $scope.alerts.push({type: 'danger', title: 'Error '+ response.status, msg: response.statusText});
+                    $scope.alerts.push({type: 'danger', title: 'Error ' + response.status, msg: response.statusText});
                 });
             } else {
                 $scope.messageBuilder = 'You have successfully updated these fields [';
@@ -48,19 +49,27 @@ angular.module('app')
                         data: $scope.artist
                     }).then(function (response) {
                         $scope.status = $scope.messageBuilder.substring(0, $scope.messageBuilder.length - 2) + "] of artist.";
-                        createUpdateTools.setAlerts([{type: 'success', title:'Successful!', msg: $scope.status}]);
+                        createUpdateTools.setAlerts([{type: 'success', title: 'Successful!', msg: $scope.status}]);
                         $location.path("/artistsOverview");
                     }, function (response) {
-                        $scope.alerts.push({type: 'danger', title: 'Error '+ response.status, msg: response.statusText});
+                        $scope.alerts.push({
+                            type: 'danger',
+                            title: 'Error ' + response.status,
+                            msg: response.statusText
+                        });
                     });
                 } else {
-                    createUpdateTools.setAlerts([{type: 'info', title: "No change!", msg: 'There have been no changes.'}]);
+                    createUpdateTools.setAlerts([{
+                        type: 'info',
+                        title: "No change!",
+                        msg: 'There have been no changes.'
+                    }]);
                     $location.path("/artistsOverview");
                 }
             }
         };
 
-        $scope.reset = function(form) {
+        $scope.reset = function (form) {
             if (form) {
                 form.$setPristine();
                 form.$setUntouched();
@@ -69,26 +78,34 @@ angular.module('app')
         };
 
         $scope.removeArtist = function () {
-            if(!$scope.artist) {
+            if (!$scope.artist) {
                 confirm('Cannot remove Artist immediately.\nArtist probably have not been saved yet.');
                 $location.path("/artistsOverview");
             } else {
-                if(confirm('You are above to completely remove Artist:\n'+ $scope.artist.name +'\n\nAre you sure?')) {
+                if (confirm('You are above to completely remove Artist:\n' + $scope.artist.name + '\n\nAre you sure?')) {
                     $http({
                         url: '/music/api/artist/' + $scope.artist.id,
                         method: "DELETE"
                     }).then(function () {
-                        createUpdateTools.setAlerts([{type: 'success', title:'Removed!', msg: 'You have successfully deleted '+ $scope.artist.name +' from system.'}]);
+                        createUpdateTools.setAlerts([{
+                            type: 'success',
+                            title: 'Removed!',
+                            msg: 'You have successfully deleted ' + $scope.artist.name + ' from system.'
+                        }]);
                         $location.path("/artistsOverview");
                     }, function () {
-                        createUpdateTools.setAlerts([{type: 'danger', title:'Cannot Remove!', msg: $scope.artist.name +' still has some albums assigned.'}]);
+                        createUpdateTools.setAlerts([{
+                            type: 'danger',
+                            title: 'Cannot Remove!',
+                            msg: $scope.artist.name + ' still has some albums assigned.'
+                        }]);
                         $location.path("/artistsOverview");
                     })
                 }
             }
         };
 
-        $scope.closeAlert = function(index) {
+        $scope.closeAlert = function (index) {
             $scope.alerts.splice(index, 1);
         };
 
